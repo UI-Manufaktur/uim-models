26ed28e15d23eea68f69646c13cd72fe5c8b0a3c
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:36:22 +0100

    Moves from uim.oop to uim.models

--- source/uim/models/attributes/entities/businessunit.d
+++ source/uim/models/attributes/entities/businessunit.d
@@ -3,7 +3,7 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.oop.attributes.entities.businessunit;
+module uim.models.attributes.entities.businessunit;
 
 @safe:
 import uim.models;
