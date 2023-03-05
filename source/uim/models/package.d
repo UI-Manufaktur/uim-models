6c15b4f319d662ef11e5f4275750963800c6c5a1
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:19:51 +0100

    Rebuilding uim.models

--- source/uim/models/package.d
+++ source/uim/models/package.d
@@ -1,1 +1,9 @@
 module uim.models;
+
+public import uim.models.attributes;
+public import uim.models.elements;
+public import uim.models.entities;
+public import uim.models.errors;
+public import uim.models.exceptions;
+public import uim.models.usecases;
+public import uim.models.values;
