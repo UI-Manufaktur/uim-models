c166f0179d513585a0386e510fb91573372e7ec4
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:22:22 +0100

    Move attributes from uim.oop to uim.models

---
+++ source/uim/models/attributes/doubles/grams/package.d
@@ -0,0 +1,11 @@
+/*********************************************************************************************************
+  Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+  License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+  Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.oop.attributes.doubles.grams;
+
+public import uim.oop.attributes.doubles.grams.gram;
+
+public import uim.oop.attributes.doubles.grams.kilo;
+public import uim.oop.attributes.doubles.grams.milli;
