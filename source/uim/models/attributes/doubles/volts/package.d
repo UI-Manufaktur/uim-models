deeacdc561475a622f26e78c97ee7cdcbe029917
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:22:27 +0100

    Move attributes from uim.oop to uim.models

---
+++ source/uim/models/attributes/doubles/volts/package.d
@@ -0,0 +1,13 @@
+/*********************************************************************************************************
+  Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+  License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+  Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.oop.attributes.doubles.volts;
+
+public import uim.oop.attributes.doubles.volts.volt;
+
+public import uim.oop.attributes.doubles.volts.kilo;
+public import uim.oop.attributes.doubles.volts.mega;
+public import uim.oop.attributes.doubles.volts.milli;
+
