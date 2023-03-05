1dd2d9531c83ddf6d5b928db2d7e7cad87a1c11d
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:37:57 +0100

    Moves from uim.oop to uim.models

--- source/uim/models/values/arrays/string_.d
+++ source/uim/models/values/arrays/string_.d
@@ -3,7 +3,7 @@
 	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
 	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.oop.values.arrays.string_;
+module uim.models.values.arrays.string_;
 
 @safe:
 import uim.models;
