b4d509cb83837a738efaf7eabb4e505c3e846f9a
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:29:03 +0100

    Moves from uim.oop to uim.models

---
+++ source/uim/models/values/maps/package.d
@@ -0,0 +1,19 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.oop.values.maps;
+
+@safe:
+import uim.oop;
+
+public import uim.oop.values.maps.map;
+public import uim.oop.values.maps.options;
+public import uim.oop.values.maps.config;
+
+/* ublic import uim.oop.values.maps.lookup;
+public import uim.oop.values.maps.customertype;
+public import uim.oop.values.maps.string_;
+public import uim.oop.values.maps.preferredcontactmethod; */
+
---
+++ source/uim/models/values/scalars/package.d
@@ -0,0 +1,12 @@
+/*********************************************************************************************************
+  Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+  License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+  Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.oop.values.scalars;
+
+public import uim.oop.values.scalars.boolean;
+public import uim.oop.values.scalars.double_;
+public import uim.oop.values.scalars.integer;
+public import uim.oop.values.scalars.long_;
+public import uim.oop.values.scalars.string_;
