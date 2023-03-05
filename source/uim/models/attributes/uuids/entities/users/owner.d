e5fea6c932af4b6fcddf7bc4f319b27504c198b0
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:23:58 +0100

    Move attributes from uim.oop to uim.models

---
+++ source/uim/models/attributes/uuids/entities/users/owner.d
@@ -0,0 +1,38 @@
+/*********************************************************************************************************
+  Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+  License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+  Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.oop.attributes.uuids.entities.users.owner;
+
+@safe:
+import uim.oop;
+
+class DOwnerIdAttribute : DEntityIdAttribute {
+  mixin(AttributeThis!("OwnerIdAttribute"));
+
+  // Initialization hook method.
+  override void initialize(DConfigurationValue configSettings = null) {
+    super.initialize(configSettings);
+
+    this
+      .name("ownerId")
+      .registerPath("ownerId");
+  }  
+}
+mixin(AttributeCalls!("OwnerIdAttribute"));
+
+///
+unittest {
+  auto attribute = new DOwnerIdAttribute;
+  assert(attribute.name == "ownerId");
+  assert(attribute.registerPath == "ownerId");
+
+  DAttribute generalAttribute = attribute;
+  assert(cast(DEntityIdAttribute)generalAttribute);
+  assert(cast(DUUIDAttribute)generalAttribute);
+  assert(!cast(DIntegerAttribute)generalAttribute);
+
+  DValue value = attribute.createValue();
+  assert(cast(DUUIDValue)value);
+}
---
+++ source/uim/models/attributes/uuids/entities/users/package.d
@@ -0,0 +1,12 @@
+/*********************************************************************************************************
+  Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+  License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+  Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.oop.attributes.uuids.entities.users;
+
+public import uim.oop.attributes.uuids.entities.users.user;
+public import uim.oop.attributes.uuids.entities.users.owner;
+public import uim.oop.attributes.uuids.entities.users.createdonbehalfby;
+public import uim.oop.attributes.uuids.entities.users.modifiedonbehalfby;
+public import uim.oop.attributes.uuids.entities.users.team;
