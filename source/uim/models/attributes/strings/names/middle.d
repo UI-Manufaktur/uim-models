00ab4dea6060ee52f3fcf8e62e79b8bf674bb38a
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:37:31 +0100

    Moves from uim.oop to uim.models

--- source/uim/models/attributes/strings/names/middle.d
+++ source/uim/models/attributes/strings/names/middle.d
@@ -3,7 +3,7 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.oop.attributes.strings.names.middle;
+module uim.models.attributes.strings.names.middle;
 
 /* any <- char <- string <- middleName
 Traits
