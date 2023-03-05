3ee6784cf49713eb9c70cf3df5ebebd8c851009c
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:22:07 +0100

    Move attributes from uim.oop to uim.models

---
+++ source/uim/models/attributes/doubles/farads/package.d
@@ -0,0 +1,12 @@
+/*********************************************************************************************************
+  Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+  License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+  Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.oop.attributes.doubles.farads;
+
+public import uim.oop.attributes.doubles.farads.farad;
+
+public import uim.oop.attributes.doubles.farads.micro;
+public import uim.oop.attributes.doubles.farads.nano;
+public import uim.oop.attributes.doubles.farads.pico;
