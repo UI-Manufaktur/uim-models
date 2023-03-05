0b9289143e2b2387bfedf74dc7ef6689dc770093
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:35:59 +0100

    Moves from uim.oop to uim.models

--- source/uim/models/attributes/doubles/farads/nano.d
+++ source/uim/models/attributes/doubles/farads/nano.d
@@ -3,7 +3,7 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.oop.attributes.doubles.farads.nano;
+module uim.models.attributes.doubles.farads.nano;
 
 /* Unit of capacitance, equivalent to 10E-9 farads
 
--- source/uim/models/attributes/doubles/hertzes/kilo.d
+++ source/uim/models/attributes/doubles/hertzes/kilo.d
@@ -3,7 +3,7 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.oop.attributes.doubles.hertzes.kilo;
+module uim.models.attributes.doubles.hertzes.kilo;
 
 /* Unit of frequency equivalent to 10E3 hertz
 
