0e9dc1725df334ef77e3caf3e4b5902a6aa9aeaa
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-05 15:36:34 +0100

    Moves from uim.oop to uim.models

--- source/uim/models/attributes/integers/integer.d
+++ source/uim/models/attributes/integers/integer.d
@@ -3,7 +3,7 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.oop.attributes.integers.integer;
+module uim.models.attributes.integers.integer;
 
 @safe:
 import uim.models;
