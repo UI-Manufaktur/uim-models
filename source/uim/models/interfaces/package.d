5c9feb8ca6aef78ad08e4bc58d61f2100310c543
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-04-27 17:13:12 +0200

    Update package.d

--- source/uim/models/interfaces/package.d
+++ source/uim/models/interfaces/package.d
@@ -1,7 +1,9 @@
 module uim.models.interfaces;
 
 public {
+  import uim.models.interfaces.attribute;
   import uim.models.interfaces.element;
   import uim.models.interfaces.entity;
+  import uim.models.interfaces.usecase;
   import uim.models.interfaces.value;
 }
