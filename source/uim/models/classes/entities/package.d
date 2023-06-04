4c4b82c5d48c2fdf52ecf6fce389a586ae56580c
UIMSolutions <ui.manufaktur@gmail.com>
2023-06-04 12:23:31 +0200

    Update

---
+++ source/uim/models/classes/entities/package.d
@@ -0,0 +1,17 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module uim.models.entities;
+
+import uim.models;
+@safe:
+
+public { 
+	import uim.models.entities.entity;
+	import uim.models.entities.entities;
+	import uim.models.entities.entity_lang;
+	import uim.models.entities.registry;
+}
+
