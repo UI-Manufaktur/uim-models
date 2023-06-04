e793051bd2363a3844672132965949854ee41570
UIMSolutions <ui.manufaktur@gmail.com>
2023-06-04 12:25:17 +0200

    Update

--- source/uim/models/classes/attributes/strings/ip4address.d
+++ source/uim/models/classes/attributes/strings/ip4address.d
@@ -3,7 +3,7 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.models.attributes.strings.ip4address;
+module uim.models.classes.attributes.strings.ip4address;
 
 /* Internet Protocol V4 Address of the form DDD.DDD.DDD.DDD
 
--- source/uim/models/classes/attributes/strings/json.d
+++ source/uim/models/classes/attributes/strings/json.d
@@ -8,7 +8,7 @@
   License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
   Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
 **********************************************************************************************************/
-module uim.models.attributes.strings.json;
+module uim.models.classes.attributes.strings.json;
 
 /* A JSON fragment contained within one string value
 
