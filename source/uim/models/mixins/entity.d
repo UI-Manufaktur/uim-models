c82c3f93f5bae18a85c83d51813377049c29f8b1
UIMSolutions <ui.manufaktur@gmail.com>
2023-06-21 08:09:56 +0200

    Update

--- source/uim/models/mixins/entity.d
+++ source/uim/models/mixins/entity.d
@@ -1,23 +1,37 @@
 module uim.models.mixins.entity;
 
-template EntityThis(string name) {
-  const char[] EntityThis = `
-    this() { super(); this.className("`~name~`"); }
-    this(UUID myId) { this().id(myId).name("`~name~`-"~this.id.toString); }
-    this(string myName) { this().name(myName); }
-    this(UUID myId, string myName) { this(myId).name(myName); }
-    this(Json aJson) { this().fromJson(aJson); }
+// #region EntityThis
+  string entityThis(string shortName, string className = null) {
+    string clName = className.length > 0 ? className : "D"~shortName;
+    return `
+      this() { super(); this.className("`~shortName~`"); }
+      this(UUID myId) { this().id(myId).name("`~shortName~`-"~this.id.toString); }
+      this(string myName) { this().name(myName); }
+      this(UUID myId, string myName) { this(myId).name(myName); }
+      this(Json aJson) { this().fromJson(aJson); }
 
-    override DEntity create() { return `~name~`; }
-  `;
-}
+      override DEntity create() { return new `~clName~`; }
+    `;
+  }
 
-template EntityCalls(string name) {
-  const char[] EntityCalls = `
-    auto `~name~`() { return new D`~name~`; } 
-    auto `~name~`(UUID myId) { return new D`~name~`(myId); } 
-    auto `~name~`(string myName) { return new D`~name~`(myName); } 
-    auto `~name~`(UUID myId, string myName) { return new D`~name~`(myId, myName); } 
-    auto `~name~`(Json json) { return new D`~name~`(json); } 
-  `;
-}
+  template EntityThis(string shortName, string className = null) {
+    const char[] EntityThis = entityThis(shortName, className);
+  }
+// #endregion EntityThis
+
+// #region EntityCalls
+  string entityCalls(string shortName, string className = null) {
+    string clName = className.length > 0 ? className : "D"~shortName;
+    return `
+      auto `~shortName~`() { return new `~clName~`; } 
+      auto `~shortName~`(UUID myId) { return new `~clName~`(myId); } 
+      auto `~shortName~`(string myName) { return new `~clName~`(myName); } 
+      auto `~shortName~`(UUID myId, string myName) { return new `~clName~`(myId, myName); } 
+      auto `~shortName~`(Json json) { return new `~clName~`(json); } 
+    `;
+  }
+
+  template EntityCalls(string shortName, string className = null) {
+    const char[] EntityCalls = entityCalls(shortName, className);
+  }
+// #endregion EntityCalls
